01d3425a9fd91ac51966165e8076a3c71a4532de
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-06-30 23:38:43 +0530

    easy

---
+++ DP/CoinChange.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    int dp[10010];
+    int func(vector<int> &coins,int amount){
+       if(amount==0) return 0;
+        if(dp[amount] != -1) return dp[amount];
+        
+        int ans = INT_MAX;
+        
+        for(auto coin : coins){
+            if(amount-coin>=0){
+                ans = min(ans+0LL,func(coins,amount-coin)+1LL);
+            }
+            
+        }  return dp[amount] = ans;
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+        int ans=func(coins,amount);
+        
+        return ans==INT_MAX?-1:ans;
+    }
+};
