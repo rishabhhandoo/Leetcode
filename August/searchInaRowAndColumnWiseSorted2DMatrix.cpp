b1607d62829d1dd496f2519e403e2c01eb8e5b3f
Rishabh Handoo <rhandoo_be20@thapar.edu>
2022-08-02 11:54:08 +0530

    medium

---
+++ August/searchInaRowAndColumnWiseSorted2DMatrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r = matrix.size();
+        // if(n==0)    return false;
+
+        int c = matrix[0].size();
+        int i = 0;
+        int j = c-1;
+        
+        while(i<r && j>=0)
+        {
+            if(matrix[i][j]>target)     j--;
+            else if(matrix[i][j]<target)    i++;
+            else    return true;
+
+        }
+            
+        return false;
+    }
+};
