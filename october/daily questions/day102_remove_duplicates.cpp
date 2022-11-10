58ccb953edc05c069531d45fa3626e062857309a
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2022-11-10 06:24:04 +0530

    easy

---
+++ october/daily questions/day102_remove_duplicates.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        
+        stack<char> st;
+        st.push(s[0]);
+        for(int i=1;i<s.size();i++)
+        {
+            if(!st.empty() && st.top()==s[i])
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+        
+        string ans = "";
+        while(!st.empty())
+        {
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+        
+    }
+};
