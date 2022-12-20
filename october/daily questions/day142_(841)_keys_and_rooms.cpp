17c38b0cfd2a4783e1066dc862332a940681d295
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2022-12-20 21:50:18 +0530

    medium

---
+++ october/daily questions/day142_(841)_keys_and_rooms.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    void dfs(vector<vector<int>>& rooms, unordered_set<int> & keys, unordered_set<int> & visited, int curr) {
+        visited.insert(curr);
+        for (int k : rooms[curr]) keys.insert(k);
+        for (int k : keys) if (visited.find(k) == visited.end()) dfs(rooms, keys, visited, k);
+    }
+    
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> keys;
+        unordered_set<int> visited;
+        dfs(rooms, keys, visited, 0);
+        return visited.size() == rooms.size();
+    }
+};
