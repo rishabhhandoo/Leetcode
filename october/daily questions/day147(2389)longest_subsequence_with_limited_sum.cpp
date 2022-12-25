6a17faeceb29c91c3f984f9761dad963f9891e74
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2022-12-25 18:04:31 +0530

    easy

---
+++ october/daily questions/day147(2389)longest_subsequence_with_limited_sum.cpp
@@ -0,0 +1,9 @@
+vector<int> answerQueries(vector<int> A, vector<int>& queries) {
+        sort(A.begin(), A.end());
+        vector<int> res;
+        for (int i = 1; i < A.size(); ++i)
+            A[i] += A[i - 1];
+        for (int& q: queries)
+            res.push_back(upper_bound(A.begin(), A.end(), q) - A.begin());
+        return res;
+    }
