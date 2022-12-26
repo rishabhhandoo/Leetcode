59fb35e46999ee04d4eac458fe0517754f0c2bb0
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2022-12-26 14:25:54 +0530

    medium

---
+++ october/daily questions/day148(55)jump_game.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(reach<i) return false;
+            
+            reach = max(reach,nums[i]+i);
+        }
+        return true;
+    }
+};
