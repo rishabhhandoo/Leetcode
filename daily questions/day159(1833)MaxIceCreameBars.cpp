05b456333ec0fae88187f6e1df633fe4ab194dda
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-01-06 19:03:28 +0530

    medium

---
+++ daily questions/day159(1833)MaxIceCreameBars.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+
+        int ans=0;
+        for(int i=0;i<costs.size();i++)
+        {
+            coins-=costs[i];
+            if(coins<0)
+            {
+                return ans;
+            }
+            ans++;
+        }
+        return ans;
+
+    }
+};
