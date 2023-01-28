276c1ab89bfcc568b40f8d3d960e46a588c8f0e7
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-01-28 20:12:20 +0530

    Restoring streak try 1

---
+++ IMP_Topics/DSU(UNION FIND)/base code for union find.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N=1e5+10;
+int parent[N];
+int size[N];
+
+void make(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+int find(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b){
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
+
+
+int main()
+{
+
+ 
+return 0;
+}
