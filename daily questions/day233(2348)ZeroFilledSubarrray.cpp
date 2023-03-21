41d873f1d86e4836d71707fb24dbb3090ffb6116
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-03-21 21:37:10 +0530

    medium

---
+++ daily questions/day233(2348)ZeroFilledSubarrray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans=0;
+        long long i=0;
+        while(i<nums.size())
+        {
+            int j=i;
+            while(j<nums.size() && nums[j]==0)
+            {
+                j++;
+                ans+=j-i;
+            }
+            if(j==i)    i++;
+            else
+                i=j;
+        }
+        return ans;
+    }
+};
