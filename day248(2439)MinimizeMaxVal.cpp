81479738bb4132c56ea416b543a466a5a6d7c2ca
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-04-05 16:19:18 +0530

    medium

---
+++ day248(2439)MinimizeMaxVal.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums) {
+    long long summ=0;      
+    int ans=0;
+    for(int i=0;i<nums.size();i++)
+    {
+        summ+=nums[i];
+        int temp = ceil(double(summ)/(i+1));
+        ans = max(ans,temp);
+    }
+    return ans;
+    }
+};
