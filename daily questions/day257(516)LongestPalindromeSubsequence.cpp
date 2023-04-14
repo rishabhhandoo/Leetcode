512c178dac7f98638e0b1a4d619e7160c73a2cb3
Rishabh4-2-2002 <rhandoo_be20@thapar.edu>
2023-04-14 20:47:25 +0530

    medium

---
+++ daily questions/day257(516)LongestPalindromeSubsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s2) {
+        string s1=s2;
+        reverse(s2.begin(),s2.end());
+
+        int n= s1.size();
+        int m = n;
+        int t[n+1][m+1];
+        
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<m+1;j++)
+            {
+                if(i==0 || j==0) t[i][j]=0;
+            }
+        }
+        
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<m+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    t[i][j] = 1+t[i-1][j-1];
+                }else{
+                    t[i][j] = max(t[i-1][j] , t[i][j-1]);
+                }
+            }
+        }
+        return t[n][m];
+    }
+};
